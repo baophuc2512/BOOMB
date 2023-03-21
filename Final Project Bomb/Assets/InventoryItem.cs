8de04f246f2738585c1cc84fa17a0b93daf1bc57
CUTEANDFUNNY\Mahir <>
2023-03-21 19:03:19 +0700

    Figured out invtory, tomorrow will finish.

---
+++ Final Project Bomb/Assets/InventoryItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour
+{
+    public Image Inv1;
+    public Image Inv2;
+    public Image Inv3;
+    public Image Inv4;
+    public Image Inv5;
+    public ScriptableData InvData;
+    public void InvtoryItemsOnStarttup()
+    {
+       
+    }
+}
