34fefdbd7b1615a768cfaa4cdfcaa75e8cd9bb2e
WIN-FMJNPG6CPVF\Administrator <nguyennhatminh21p@gmail.com>
2023-03-25 09:07:23 +0700

    fixbug

--- Final Project Bomb/Assets/InventoryItem.cs
+++ Final Project Bomb/Assets/InventoryItem.cs
@@ -6,60 +6,33 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour
-{   
-    int i = 2;
-    public Image Inv1;
-    public Image Inv2;
-    public Image Inv3;
-    public Image Inv4;
-    public Image Inv5;
-    public Image hightlight1;
-    public Image hightlight2;
-    public Image hightlight3;
-    public Image hightlight4;
-    public Image hightlight5;
+{
+    public void Awake()
+    {
+        Time.timeScale = 1;
+    }
+    public List<Image> hightlights;
+    public GameObject player;
+    private SelectItem selectItem;
     public ScriptableData InvData;
+
+    private void Start()
+    {
+        selectItem = player.GetComponent<SelectItem>();
+    }
     void Update()
     {
-       
-       if(Input.GetKeyUp(KeyCode.E))
-        {
-            
-             if (i ==2)
-                {
-                hightlight1.transform.gameObject.SetActive(false);
-                hightlight2.transform.gameObject.SetActive(true);
-                ++i;
-                }
-            else if (i == 3)
+        
+        hightLightDo(selectItem.currentSelectItem);
+    }
+
+    public void hightLightDo(int currentHightLight)
+    {
+        for (int tmp = 0; tmp < hightlights.Count;++tmp)
             {
-                hightlight2.transform.gameObject.SetActive(false);
-                hightlight3.transform.gameObject.SetActive(true);
-                ++i;
-            }
-            else if (i == 4)
-            {
-                hightlight3.transform.gameObject.SetActive(false);
-                hightlight4.transform.gameObject.SetActive(true);
-                ++i;
-            }
-            else if (i == 5)
-            {
-                hightlight4.transform.gameObject.SetActive(false);
-                hightlight5.transform.gameObject.SetActive(true);
-                i = 1;
-            }       
-            else if (i==1)
-            {
-                hightlight5.transform.gameObject.SetActive(false);
-                hightlight1.transform.gameObject.SetActive(true);
-                ++i;
-            }
-            
-            
+            hightlights[tmp].transform.gameObject.SetActive(false);
         }
         
-        
-
+        hightlights[currentHightLight].transform.gameObject.SetActive(true);
     }
 }
--- Final Project Bomb/Assets/Script/Minh/ExitGame.cs
+++ Final Project Bomb/Assets/Script/Minh/ExitGame.cs
@@ -6,8 +6,7 @@ public class ExitGame : MonoBehaviour
 {
     public void QuitGame()
     {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+
+        Application.Quit();
     }
 }
